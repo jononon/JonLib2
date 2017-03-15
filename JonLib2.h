172e8bf32d40a0d0b9712e15fc809eb4ac870bee
Jonathan Damico <jj_damico@yahoo.com>
2017-03-14 17:26:11 -0700

    move things around, have includes file

---
+++ JonLib2.h
@@ -0,0 +1,8 @@
+#pragma systemFile
+
+#define MOVE_TIMEOUT 1000 //timeout for "auto" moves
+#define THRESHOLD_COEFF 2 //expanded size of threshold for timeout
+
+#include "Includes/PID.h"
+#include "Includes/Movement.h"
+#include "Includes/Gyro.h"
